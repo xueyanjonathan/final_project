78fdcbfd41d6918761f69a5ebf7e2d53e25ce0be
Jonathan <jonathan@iMac9.local>
2019-05-30 14:49:11 -0400

    Created the app icon

--- LaundryApp/LaundryApp/Laundry.swift
+++ LaundryApp/LaundryApp/Laundry.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Jonathan on 5/16/19.
 //  Copyright © 2019 Jonathan. All rights reserved.
-//  This 
+//  This file creates the struct for laundry. Each person will be in the following format.
 
 import Foundation
 
